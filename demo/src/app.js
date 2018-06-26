1c109b6455603ed5254daa2739b2f83b02a7f333
wangyichen1064431086 <1064431086@qq.com>
2018-06-26 15:14:31 +0800

    expand the sticky to all width

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,9 +1,9 @@
-//import SearchBar from '../../src/js/SearchBar.js';
-import SearchBar from '../../build/index.es';
+import SearchBar from '../../src/js/SearchBar.js';
+//import SearchBar from '../../build/index.es';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <SearchBar postUrl="#" sticky={false}/>,
+  <SearchBar postUrl="#" sticky={true}/>,
   document.getElementById('root')
 );
